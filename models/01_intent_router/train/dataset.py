935a4d7c5762e3dd5ae04924de6aec22b45ead6e
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-08 12:52:47 +0530

    feat: implemented the __len__ function to return the text length and __getitem__ to Retrieves a single sample, tokenizes it, and returns it as a dictionary of tensors.

--- models/01_intent_router/train/dataset.py
+++ models/01_intent_router/train/dataset.py
@@ -18,3 +18,27 @@ class IntentDataset(Dataset):
         self.max_length = max_length
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+
+            text = str(self.texts[idx])
+            label = self.labels[idx]
+
+            encoding = self.tokenizer.encode_plus(
+                text,
+                add_special_tokens=True,
+                max_length=self.max_length,
+                padding='max_length',
+                truncation=True,
+                return_attention_mask=True,
+                return_tensors='pt'
+            )
+
+            return{
+                "input_ids" : encoding["input_ids"].flatten(),
+                "attention_mask" : encoding["attention_mask"].flatten(),
+                "labels" : torch.tensor(label, dtype=torch.long)
+            }

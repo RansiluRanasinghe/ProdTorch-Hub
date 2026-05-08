e62429afe3e2832165c25801ecfc63967f9277ed
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-08 15:50:41 +0530

    feat: implemented on event lister load_model function to Loads the model from the local SQLite MLflow registry when the server starts.

--- models/serve/app.py
+++ models/serve/app.py
@@ -12,3 +12,42 @@ app = FastAPI(
     description="Production API for classifying user prompts into system intents.",
     version="1.0.0"
 )
+
+model = None
+tokenizer = None
+decvice = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+@app.on_event("startup")
+def load_model():
+
+    global model, tokenizer
+
+    try:
+        print("Initializing Tokenizer")
+        tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased", use_fast=True)
+
+        print("Locating MLFlow Database")
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        root_dir = os.path.abspath(os.path.join(current_dir, "../../../"))
+        db_path = os.path.join(root_dir, "mlflow.db")
+
+        if not os.path.exists(db_path):
+            print(f"WARNING: Database not found at {db_path}")
+            return
+        
+        mlflow.set_tracking_uri(f"sqlite:///{db_path}")
+
+        print("Loading model from MLflow Registry (Version 1)...")
+
+        model_uri = "models:/AgenticIntentRouter/1"
+
+        model = mlflow.pytorch.load_model(model_uri)
+        model.to(decvice)
+        model.eval()
+
+        print("Model loaded successfully and ready for inference!")
+
+    except Exception as e:
+        print(f"Error loading model: {e}")
+    

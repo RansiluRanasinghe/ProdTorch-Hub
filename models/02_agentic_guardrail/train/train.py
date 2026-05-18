d01bb3a622b1441a7a8d99d607a946f2e6681f54
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-18 15:28:59 +0530

    feat: implemented run_training function to do the training and calculate the metrics

--- models/02_agentic_guardrail/train/train.py
+++ models/02_agentic_guardrail/train/train.py
@@ -48,4 +48,67 @@ def train_epoch(model, data_loader, loss_fn, optimizer, device, scheduler):
     acc = accuracy_score(all_labels, all_preds)
     f1 = f1_score(all_labels, all_preds, average='binary')
 
-    return acc, f1, (total_loss / len(data_loader))    
+    return acc, f1, (total_loss / len(data_loader))
+
+def run_training():
+
+    print("Initializing Agentic Guardrail Training Pipeline...")
+
+    MODEL_NAME = "microsoft/deberta-v3-xsmall"
+    EPOCHS = 2,
+    BATCH_SIZE = 16
+    LEARNING_RATE = 2e-5
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Hardware: Training on {DEVICE}")
+
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
+    db_path = os.path.join(root_dir, "mlflow.db")
+    mlflow.set_tracking_uri(f"sqlite:///{db_path}")
+    mlflow.set_experiment("Guardrail_Production")
+
+    with mlflow.start_run():
+        mlflow.log_params({
+            "model_architecture": "DeBERTa-v3-xsmall",
+            "epochs": EPOCHS,
+            "batch_size": BATCH_SIZE,
+            "learning_rate": LEARNING_RATE,
+            "device": str(DEVICE)
+        })
+
+        train_ds, test_ds = load_security_data(model_name=MODEL_NAME)
+
+        print("\nSubsetting training data to 500 samples for local speed...")
+        train_ds.texts = train_ds.texts[:500]
+        train_ds.labels = train_ds.labels[:500]
+
+        train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+
+        model = GuardrailClassifier(model_name=MODEL_NAME, num_labels=2)
+        model.to(DEVICE)
+
+        optimizer = AdamW(model.parameters(), lr=LEARNING_RATE)
+        total_steps = len(train_loader) * EPOCHS
+        schedular = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+
+        loss_fn = nn.CrossEntropyLoss().to(DEVICE)
+
+        for epoch in range(EPOCHS):
+            print(f"\n--- Epoch {epoch + 1}/{EPOCHS} ---")
+
+            train_acc, train_f1, train_loss = train_epoch(model, train_loader, loss_fn, optimizer, DEVICE, schedular)
+
+            mlflow.log_metric("train_accuracy", train_acc, step=epoch)
+            mlflow.log_metric("train_f1_score", train_f1, step=epoch)
+            mlflow.log_metric("train_loss", train_loss, step=epoch)
+
+            print(f"Loss: {train_loss:.4f} | Accuracy: {train_acc:.4f} | F1-Score (Unsafe): {train_f1:.4f}")
+
+        print("\nSaving and Registering model to MLflow...")
+        mlflow.pytorch.log_model(
+            pytorch_model=model,
+            artifact_path="agentic_guardrail_model",
+            registered_model_name="AgenticGuardrail",
+            pip_requirements=["torch", "transformers", "mlflow", "datasets"]
+        )
+        
+        print("Guardrail model training complete and registered!")    

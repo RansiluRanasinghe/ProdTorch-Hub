092b838f5b79c2198eef5264510157e952680e55
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-18 14:44:07 +0530

    feat: Implemented the forward pass for binary classifier

--- models/02_agentic_guardrail/train/model_arch.py
+++ models/02_agentic_guardrail/train/model_arch.py
@@ -12,3 +12,16 @@ class GuardrailClassifier(nn.Module):
 
         self.dropout = nn.Dropout(dropout_rate)
         self.classifier = nn.Linear(self.config.hidden_size, num_labels)
+
+    def forward(self, input_ids, attention_mask):
+
+        outputs = self.deberta(input_ids=input_ids, attention_mask=attention_mask)
+
+        hidden_state = outputs.last_hidden_state
+
+        cls_token_state = hidden_state[:, 0, :]
+
+        cls_token_state = self.dropout(cls_token_state)
+        logits = self.classifier(cls_token_state)
+
+        return logits    

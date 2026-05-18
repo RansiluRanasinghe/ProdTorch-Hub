ce6843eb22e006a107ff7f1b85e723292fb956c8
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-18 15:20:14 +0530

    feat: implemented train_epoch function to define the training loop

--- models/02_agentic_guardrail/train/train.py
+++ models/02_agentic_guardrail/train/train.py
@@ -15,3 +15,37 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from model_arch import GuardrailClassifier
 from dataset import load_security_data
+
+def train_epoch(model, data_loader, loss_fn, optimizer, device, scheduler):
+
+    model.train()
+    total_loss = 0
+    all_preds = []
+    all_labels = []
+
+    for batch in tqdm(data_loader, desc="Training"):
+        optimizer.zero_grad()
+
+        input_ids = batch["input_ids"].to(device)
+        attention_mask = batch["attention_mask"].to(device)
+        labels = batch["labels"].to(device)
+
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+        _, preds = torch.max(outputs, dim=1)
+
+        loss = loss_fn(outputs, labels)
+
+        all_preds.extend(preds.cpu().numpy())
+        all_labels.extend(labels.cpu().numpy())
+        total_loss += loss.item()
+
+        loss.backward()
+
+        nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+        scheduler.step()
+
+    acc = accuracy_score(all_labels, all_preds)
+    f1 = f1_score(all_labels, all_preds, average='binary')
+
+    return acc, f1, (total_loss / len(data_loader))    
